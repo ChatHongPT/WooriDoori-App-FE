5b56b707f1d309ae79c67d39d8de4268801ed9b7
menzzi <minmin3927@naver.com>
2025-10-23 16:56:00 +0900

    [feat] #1 - doori 이미지들 추가

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -21,9 +21,38 @@ import transfer from './transfer.png';
 import residence from './residence.png';
 import communication from './communication.png';
 import mart from './mart.png';
-
+import doori_angry from './doori/doori_angry.png';
+import doori_annoyed from './doori/doori_annoyed.png';
+import doori_card from './doori/doori_card.png';
+import doori_celebrating from './doori/doori_celebrating.png';
+import doori_cheering from './doori/doori_cheering.png';
+import doori_coffee from './doori/doori_coffee.png';
+import doori_coin_warning from './doori/doori_coin_warning.png';
+import doori_cool from './doori/doori_cool.png';
+import doori_counting_coins from './doori/doori_counting_coins.png';
+import doori_crying from './doori/doori_crying.png';
+import doori_drawing from './doori/doori_drawing.png';
+import doori_face1 from './doori/doori_face1.png';
+import doori_face2 from './doori/doori_face2.png';
+import doori_face3 from './doori/doori_face3.png';
+import doori_face4 from './doori/doori_face4.png';
+import doori_face5 from './doori/doori_face5.png';
+import doori_facepalm from './doori/doori_facepalm.png';
+import doori_frustrated from './doori/doori_frustrated.png';
+import doori_ghost from './doori/doori_ghost.png';
+import doori_heart from './doori/doori_heart.png';
+import doori_normal from './doori/doori_normal.png';
+import doori_pouting from './doori/doori_pouting.png';
+import doori_question from './doori/doori_question.png';
+import doori_report from './doori/doori_report.png';
+import doori_thinking from './doori/doori_thinking.png';
+import doori_wallet from './doori/doori_wallet.png';
+import doori_writing from './doori/doori_writing.png';
 
 export const img ={
     cardIcon, BsX, Vector, calendarB, calendarW, cardB, cardW, homeB, homeW, menuB, menuW, education, traffic, etc, 
-    hospital, shopping, entertainment, food, travel, transfer, residence, communication, mart,
+    hospital, shopping, entertainment, food, travel, transfer, residence, communication, mart, doori_angry, doori_annoyed,
+    doori_card, doori_celebrating, doori_cheering, doori_coffee, doori_coin_warning, doori_cool, doori_counting_coins, doori_crying,
+    doori_drawing, doori_face1, doori_face2, doori_face3, doori_face4, doori_face5, doori_facepalm, doori_frustrated, doori_ghost,
+    doori_heart, doori_normal, doori_pouting, doori_question, doori_report, doori_thinking, doori_wallet, doori_writing,
 }
