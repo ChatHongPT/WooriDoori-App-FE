c9f1bb27fbfc7e9c5e1901dcd6d55ecea193aa27
menzzi <minmin3927@naver.com>
2025-10-23 10:38:51 +0900

    [feat] #1 - NavBar 컴포넌트 생성

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -1,7 +1,15 @@
 import cardIcon from './card-icon.svg';
 import BsX from './BsX.png';
 import Vector from './Vector.png';
+import calendarB from './calendar(B).png';
+import calendarW from './calendar(W).png';
+import cardB from './card(B).png';
+import cardW from './card(W).png';
+import homeB from './home(B).png';
+import homeW from './home(W).png';
+import menuB from './menu(B).png';
+import menuW from './menu(W).png';
 
 export const img ={
-    cardIcon, BsX, Vector,
+    cardIcon, BsX, Vector, calendarB, calendarW, cardB, cardW, homeB, homeW, menuB, menuW,
 }
