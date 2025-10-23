8f7b600ae3336850594c030d502153298102d0de
menzzi <minmin3927@naver.com>
2025-10-23 16:15:49 +0900

    [feat] #1 - 카테고리 관련 컴포넌트 생성

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -9,7 +9,21 @@ import homeB from './home(B).png';
 import homeW from './home(W).png';
 import menuB from './menu(B).png';
 import menuW from './menu(W).png';
+import education from './education.png';
+import traffic from './traffic.png';
+import etc from './etc.png';
+import hospital from './hospital.png';
+import shopping from './shopping.png';
+import entertainment from './entertainment.png';
+import food from './food.png';
+import travel from './travel.png';
+import transfer from './transfer.png';
+import residence from './residence.png';
+import communication from './communication.png';
+import mart from './mart.png';
+
 
 export const img ={
-    cardIcon, BsX, Vector, calendarB, calendarW, cardB, cardW, homeB, homeW, menuB, menuW,
+    cardIcon, BsX, Vector, calendarB, calendarW, cardB, cardW, homeB, homeW, menuB, menuW, education, traffic, etc, 
+    hospital, shopping, entertainment, food, travel, transfer, residence, communication, mart,
 }
