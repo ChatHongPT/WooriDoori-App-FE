d5dd887d3795c0c51e5b925468ff03e0d1739fed
songhajang <angpan2004@gmail.com>
2025-10-24 08:39:45 +0900

    [feat] #1 - 이미지 추가

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -1,8 +1,9 @@
 import cardIcon from './card-icon.svg';
 import DeleteIcon from './delete.png';
 import EditIcon from './edit.png';
+import grayCheckRightIcon from './gray_chec_right.png';
 
 export const img ={
     cardIcon,
-    DeleteIcon, EditIcon,
+    DeleteIcon, EditIcon, grayCheckRightIcon
 }
