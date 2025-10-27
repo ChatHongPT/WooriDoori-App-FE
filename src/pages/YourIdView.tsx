815e2b3c85077c1904e2e9db904212086722250e
godnowoon <nowoon0426@naver.com>
2025-10-27 19:32:24 +0900

    [chore]/#9 - 아이디 마스킹 롤백

--- src/pages/YourIdView.tsx
+++ src/pages/YourIdView.tsx
@@ -4,28 +4,18 @@ import DefaultDiv from "@/components/default/DefaultDiv";
 import Title1 from "@/components/title/Title1";
 
 const YourIdView = () => {
-  // 예시: 백엔드에서 받아오는 이메일
-  const email = "hongseok@gmail.com";
-
-  // 이메일 마스킹 함수 (처음 3자리만 보여주고 나머지 * 처리)
-  const maskEmail = (email: string) => {
-    const [name, domain] = email.split("@");
-    if (name.length <= 3) return `${name[0]}***@${domain}`;
-    const visible = name.slice(0, 3); // 처음 3자리
-    const masked = "*".repeat(name.length - 3); // 나머지 마스킹
-    return `${visible}${masked}@${domain}`;
-  };
-
   return (
     <DefaultDiv>
       <div className="h-16" />
       <img src={img.wooridoorilogo} alt="" className="w-60 mx-auto" />
       <div className="h-8" />
 
-      <div className="text-center">
+      <div>
         <Title1 text="아이디 찾기" />
         <div className="h-4" />
-        <h3>회원님의 아이디를 찾았어요👏</h3>
+        <h3 className="text-center">
+          회원님의 아이디를 찾았어요👏
+        </h3>
 
         <div className="h-16" />
         <div className="relative w-[300px] h-[250px] mx-auto">
@@ -35,7 +25,7 @@ const YourIdView = () => {
             className="w-full h-full object-none"
           />
           <p className="absolute left-1/2 top-1/4 -translate-x-1/2 text-white text-3xl font-bold">
-            {maskEmail(email)}
+            example@gmail.com
           </p>
         </div>
 
