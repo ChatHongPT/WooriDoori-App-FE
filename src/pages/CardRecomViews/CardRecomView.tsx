c3ad21806872c158fed78c5dd24d2b95110b76a6
godnowoon <nowoon0426@naver.com>
2025-12-03 10:16:39 +0900

    [style] #85 - 변수 이름 개선

--- src/pages/CardRecomViews/CardRecomView.tsx
+++ src/pages/CardRecomViews/CardRecomView.tsx
@@ -58,7 +58,7 @@ const CardRecomView = () => {
     // 리포트의 월 정보를 "YYYY.MM" 형식으로 변환
     if (reportMonth) {
       const now = new Date();
-      const fakeHistoryList = [{
+      const thisMonthHistoryList = [{
         goalStartDate: `${now.getFullYear()}-${String(now.getMonth()+1).padStart(2,'0')}-01`
       }];
 
@@ -67,7 +67,7 @@ const CardRecomView = () => {
           year: now.getFullYear(),
           month: now.getMonth() + 1,
           from: "report",
-          historyList: fakeHistoryList
+          historyList: thisMonthHistoryList
         }
       });
     } else {
